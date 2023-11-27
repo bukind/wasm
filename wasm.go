98f9840faaeead9aaadcce9ab701cf40efeef6d5
Dmitry Bukin <bukind@indoaustinvestments.com>
2023-11-27 22:03:54 +1100

    Improve ClassList handling

--- wasm.go
+++ wasm.go
@@ -74,15 +74,55 @@ func (e *EventListener) Remove(elt js.Value) {
 	elt.Call("removeEventListener", e.name, e.fn)
 }
 
-func GetClassList(obj js.Value) (js.Value, int) {
+// ClassList represents CSS class list of an element.
+type ClassList js.Value
+
+// GetClassList returns a ClassList of the object.
+func GetClassList(obj js.Value) ClassList {
 	clist := obj.Get("classList")
 	if clist.Type() == js.TypeUndefined {
-		return clist, 0
+		return ClassList(clist)
 	}
-	if len := clist.Get("length"); len.Type() == js.TypeNumber {
-		return clist, len.Int()
+	if len := clist.Get("length"); len.Type() == js.TypeNumber && len.Int() > 0 {
+		return ClassList(clist)
 	}
-	return js.Undefined(), 0
+	return ClassList(js.Undefined())
+}
+
+// Contains returns true if a class list contains the specific class.
+func (c ClassList) Contains(s string) bool {
+	clist := js.Value(c)
+	if clist.Type() == js.TypeUndefined {
+		return false
+	}
+	return clist.Call("contains", s).Bool()
+}
+
+// Add adds the class to the class list.
+func (c ClassList) Add(s string) {
+	clist := js.Value(c)
+	if clist.Type() == js.TypeUndefined {
+		return
+	}
+	clist.Call("add", s)
+}
+
+// Remove removes the class from the class list.
+func (c ClassList) Remove(s string) {
+	clist := js.Value(c)
+	if clist.Type() == js.TypeUndefined {
+		return
+	}
+	clist.Call("remove", s)
+}
+
+// String returns the string representation of the class list.
+func (c ClassList) String() string {
+	clist := js.Value(c)
+	if clist.Type() == js.TypeUndefined {
+		return ""
+	}
+	return js.Value(c).Get("value").String()
 }
 
 // Dbg returns a human-readable representation of a js.Value, useful for debugging.
@@ -97,8 +137,9 @@ func Dbg(v js.Value) string {
 		if typ := v.Get("type"); typ.Type() != js.TypeUndefined {
 			fmt.Fprintf(sb, " type=%s", typ)
 		}
-		if clist, n := GetClassList(v); n > 0 {
-			fmt.Fprintf(sb, " cls=%s", clist.Get("value"))
+		clist := GetClassList(v)
+		if cls := clist.String(); cls != "" {
+			fmt.Fprintf(sb, " cls=%s", cls)
 		}
 		sb.WriteString(">")
 		return sb.String()
